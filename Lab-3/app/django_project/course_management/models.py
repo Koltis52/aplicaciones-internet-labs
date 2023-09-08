3c1d3d254bd25f3f31999774ab14f12a34632709
Ulises Munñoz <koltis52@gmail.com>
2023-09-08 10:05:13 -0300

    Update models.py

--- Lab-3/app/django_project/course_management/models.py
+++ Lab-3/app/django_project/course_management/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Asignaturas(models.Model):
     nombre = models.CharField(max_lenght=30)
     codigo = models.IntegerField()
+    alumnos = models.ManyToManyField(Alumnos)
     
 class Alumnos(models.Model):
     nombre = models.CharField(max_lenght=30)
     apellido_paterno = models.CharField(max_lenght=30)
     apellido_materno = models.CharField(max_lenght=30)
     fecha_de_nacimiento = models.DateField()
+    pass
