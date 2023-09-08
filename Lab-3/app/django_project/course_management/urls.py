f98bcad331f0ae57002c6165e91ea75ce4ba37a1
Ulises Munñoz <koltis52@gmail.com>
2023-09-08 11:21:25 -0300

    Probando

--- Lab-3/app/django_project/course_management/urls.py
+++ Lab-3/app/django_project/course_management/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('xd/', views.current_datetime),
+    path('crear asignatura/', views.crear_asignatura),
     path('', views.index, name="index"),
 ]
--- Lab-3/app/django_project/course_management/views.py
+++ Lab-3/app/django_project/course_management/views.py
@@ -5,6 +5,9 @@ import datetime
 def index(request):
 	return render(request, 'index.html')
 
+def crear_asignatura(request):
+    return render(request, 'crear_asignatura.html')
+
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
