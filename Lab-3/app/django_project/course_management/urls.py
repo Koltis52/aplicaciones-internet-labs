4a427cf1991b57b66263cdb0fe5b417e675a3c1c
Ulises Munñoz <koltis52@gmail.com>
2023-09-08 11:13:04 -0300

    Intentando

--- Lab-3/app/django_project/course_management/urls.py
+++ Lab-3/app/django_project/course_management/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('xd/', views.current_datetime),
     path('', views.index, name="index"),
 ]
--- Lab-3/app/django_project/course_management/views.py
+++ Lab-3/app/django_project/course_management/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 def index(request):
 	return render(request, 'index.html')
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
