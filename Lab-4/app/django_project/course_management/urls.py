15ef47a82cee80e4cc4388e66227c1b06ca9f116
Ulises Munñoz <koltis52@gmail.com>
2023-10-08 00:02:36 -0300

    Todavía falta

--- Lab-4/app/django_project/course_management/urls.py
+++ Lab-4/app/django_project/course_management/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('random/', views.peticion_random, name='random'),
     path('', views.index, name="index"),
 ]
--- Lab-4/app/django_project/course_management/views.py
+++ Lab-4/app/django_project/course_management/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from .models import Course, Student
+from django.http import HttpResponse
+import requests
 
 def index(request):
     courses = Course.objects.all()
     return render(request, 'index.html',{'courses':courses})
+
+def peticion_random(request):
+    url = "https://moviesdatabase.p.rapidapi.com/titles/random"
+
+    headers = {
+	    "X-RapidAPI-Key": "6ab30a3e29msh99f48e91eab357cp14a8e5jsnedab8a8b1c24",
+	    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return HttpResponse(response)
