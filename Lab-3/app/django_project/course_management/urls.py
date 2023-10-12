85a31b4500825feacc67f025bb6e240323a911ea
Ulises Munñoz <koltis52@gmail.com>
2023-10-12 00:06:54 -0300

    Update urls.py

--- Lab-3/app/django_project/course_management/urls.py
+++ Lab-3/app/django_project/course_management/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('eliminar asignatura', views.eliminar_asignatura),
+    path('eliminar asignatura/', views.eliminar_asignatura),
     path('crear asignatura/', views.crear_asignatura),
     path('', views.index, name="index"),
 ]
