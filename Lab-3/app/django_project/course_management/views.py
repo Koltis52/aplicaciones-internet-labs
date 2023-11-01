7da47e9421e486652855e0f2f504ef3b5b26c689
Ulises Munñoz <koltis52@gmail.com>
2023-11-01 18:20:34 -0300

    Bien

--- Lab-3/app/django_project/course_management/views.py
+++ Lab-3/app/django_project/course_management/views.py
@@ -67,6 +67,37 @@ def agregar_alumnos(request, nombre_a):
         alumnos = Alumnos.objects.all()
     return render(request, 'agregar_alumnos.html', {'alumnos':alumnos})
 
+def modificar_alumnos(request):
+    alumnos = Alumnos.objects.all()
+    return render(request, 'modificar_alumnos.html', {'alumnos':alumnos})
+
+def modificar_alumno(request, nombre_a):
+    form = AlumnoForm(request.POST)
+    if form.is_valid():
+        b = Alumnos.objects.get(nombre=nombre_a)
+        b.nombre = form.cleaned_data['nombre_alumno']
+        b.apellido_paterno = form.cleaned_data['apellido_paterno']
+        b.apellido_materno = form.cleaned_data['apellido_materno']
+        b.fecha_de_nacimiento = form.cleaned_data['fecha_de_nacimiento']
+        b.save()
+        return HttpResponseRedirect("http://localhost:8080/modificar alumnos/")
+    else:
+        form = AlumnoForm()
+    return render(request, 'modificar_alumno.html',{'form': form})
+
+def quitar_alumnos(request, nombre_a):
+    if request.method == "POST":
+        for element in request.POST.getlist('lista_alumnos'):
+            b = Alumnos.objects.get(nombre=element)
+            c = Asignaturas.objects.get(nombre=nombre_a)
+            c.alumnos.remove(b)
+        return HttpResponseRedirect("http://localhost:8080/")
+    else:
+        b = Asignaturas.objects.get(nombre=nombre_a)
+        alumnos = b.alumnos.all()
+    return render(request, 'quitar_alumnos.html', {'alumnos':alumnos})
+
+
 def eliminar_alumno(request):
     if request.method == "POST":
         body_data = json.loads(request.body)
