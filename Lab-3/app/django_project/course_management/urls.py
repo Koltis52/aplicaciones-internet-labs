f41f13aa44a142db14bf2f346a4edc3bdfd5ec39
Ulises Munñoz <koltis52@gmail.com>
2023-11-01 14:35:33 -0300

    Falta

--- Lab-3/app/django_project/course_management/urls.py
+++ Lab-3/app/django_project/course_management/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('eliminar asignatura/', views.eliminar_asignatura),
     path('asignatura eliminada/', views.asignatura_eliminada),
     path('crear asignatura/', views.crear_asignatura),
+    path('modificar asignatura/<nombre_a>/', views.modificar_asignatura),
     path('crear alumnos/', views.crear_alumnos),
     path('', views.index, name="index"),
 ]
--- Lab-3/app/django_project/course_management/views.py
+++ Lab-3/app/django_project/course_management/views.py
@@ -2,7 +2,7 @@ import json
 from django.shortcuts import render
 from .models import Asignaturas, Alumnos
 from .forms import AsignaturaForm, AlumnoForm
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 
 def index(request):
     asignaturas = Asignaturas.objects.all()
@@ -20,6 +20,18 @@ def crear_asignatura(request):
     else:
         form = AsignaturaForm()
     return render(request, 'crear_asignatura.html',{'form': form})
+
+def modificar_asignatura(request, nombre_a):
+    form = AsignaturaForm(request.POST)
+    if form.is_valid():
+        b = Asignaturas.objects.get(nombre=nombre_a)
+        b.nombre = form.cleaned_data['nombre_asignatura']
+        b.codigo = form.cleaned_data['codigo_asignatura']
+        b.save()
+        return HttpResponseRedirect("http://localhost:8080/")
+    else:
+        form = AsignaturaForm()
+    return render(request, 'modificar_asignatura.html',{'form': form})
 
 def eliminar_asignatura(request):
     body_data = json.loads(request.body)
